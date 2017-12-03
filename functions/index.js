24e0de427ea527771282978993b848c92cf61be0
cuni0716 <rcuni8@gmail.com>
2017-12-03 23:24:24 +0100

    remove unnecessary code

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,14 @@
 const functions = require('firebase-functions');
 
 
-exports.sitemap = functions.https.onRequest((req, res) => {
+exports.sitemap = functions.https.onRequest((req, res) =>
   res.format({ 'text/xml': () => res.send(`
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       <url><loc>https://ramoncuni.com</loc><priority>1.0</priority></url>
       <url><loc>https://ramoncuni.com/cv</loc><priority>1.0</priority></url>
     </urlset>
-  `) });
-});
+  `) }));
 
-exports.robots = functions.https.onRequest((req, res) => {
-  res.format({ 'text/plain': () => res.send('User-agent: *\nDisallow: /things') });
-});
+
+exports.robots = functions.https.onRequest((req, res) =>
+  res.format({ 'text/plain': () => res.send('User-agent: *\nDisallow: /things') }));
